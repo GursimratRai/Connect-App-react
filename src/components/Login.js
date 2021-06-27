0e122321b239f0b2617d97dfc5aa205455bdf45d
Gursimrat Rai <gursimratsinghrai@gmail.com>
2021-06-27 23:44:18 +0530

    Form handling : controlled component (using state)

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,35 +1,70 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
-    constructor(){
-        super();
-        //this is a uncontrolled component form handling.
-        //In these data is not managed by react state but managed by react-dom.
-        this.emailInputRef = React.createRef();
-        this.passwordInputRef = React.createRef();
-    }
-    handleFormSubmit = (e) => {
+  constructor() {
+    super();
+    //this is a uncontrolled component form handling.
+    //In these data is not managed by react state but managed by react-dom.
+    // this.emailInputRef = React.createRef();
+    // this.passwordInputRef = React.createRef();
 
-        e.preventDefault();
-        console.log('this.emailInputRef',this.emailInputRef);
-        console.log('this.passwordInputRef',this.passwordInputRef);
-    }
-    render() {
-        return (
-            <form className='login-form'>
-                <span className='login-signup-header'>Log In</span>
-                <div className='field'>
-                    <input type='email' placeholder='Email' required ref={this.emailInputRef} />
-                </div>
-                <div className='field'>
-                <input type='password' placeholder='Password' required ref={this.passwordInputRef} />
-                </div>
-                <div className='field'>
-                    <button onClick={this.handleFormSubmit}>Log In</button>
-                </div>
-            </form>      
-        );
-    }
+    //this is a controlled component form handling.
+    //Mostly used.
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleEmailChange = (e) => {
+    //   console.log(e.target.value);
+      this.setState({
+          email:e.target.value
+      });
+  };
+
+  handlePasswordChange = (e) => {
+    // console.log(e.target.value);
+    this.setState({
+        password:e.target.value
+    });
+   };
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    // console.log('this.emailInputRef', this.emailInputRef);
+    // console.log('this.passwordInputRef', this.passwordInputRef);
+    console.log('this.state',this.state);
+  };
+  render() {
+    return (
+      <form className="login-form">
+        <span className="login-signup-header">Log In</span>
+        <div className="field">
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            // ref={this.emailInputRef}
+            onChange = {this.handleEmailChange}
+            value={this.state.email}
+          />
+        </div>
+        <div className="field">
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            // ref={this.passwordInputRef}
+            onChange={this.handlePasswordChange}
+            value={this.state.password}
+          />
+        </div>
+        <div className="field">
+          <button onClick={this.handleFormSubmit}>Log In</button>
+        </div>
+      </form>
+    );
+  }
 }
 
-export default Login
+export default Login;
