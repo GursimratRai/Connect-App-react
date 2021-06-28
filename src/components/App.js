528ac138dc09099edbee96fd11d480f775617d33
Gursimrat Rai <gursimratsinghrai@gmail.com>
2021-06-28 22:41:19 +0530

    Add Settings functionality.

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,9 @@ import PropTypes from 'prop-types';
 import * as jwtDecode from 'jwt-decode';
 
 import { fetchPosts } from '../actions/posts';
-import { Home, Navbar, Page404, Login, Signup } from './';
+import { Home, Navbar, Page404, Login, Signup , Settings } from './';
 import { authenticateUser } from '../actions/auth';
 
-const Settings = () => <div> settings </div>;
 const PrivateRoute = (privateRouteToProps) => {
   const { isLoggedIn, path, component: Component } = privateRouteToProps;
   return (
