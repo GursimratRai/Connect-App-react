42fdae260167463092f6178e205bfecd32647889
Gursimrat Rai <gursimratsinghrai@gmail.com>
2021-06-28 15:17:19 +0530

    Implemented Sign up functionality.

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,22 +1,121 @@
-import { APIUrls } from "../helpers/urls";
-import { LOGIN_START } from "./actionTypes";
-import { getFormBody } from "../helpers/utils";
+import { APIUrls } from '../helpers/urls';
+import {
+  LOGIN_START,
+  LOGIN_SUCCESS,
+  LOGIN_FAILED,
+  SIGNUP_START,
+  SIGNUP_FAILED,
+  SIGNUP_SUCCESS,
+  AUTHENTICATE_USER,
+  LOG_OUT,
+} from './actionTypes';
+import { getFormBody } from '../helpers/utils';
 
-export function startLogin(){
+export function startLogin() {
+  return {
+    type: LOGIN_START,
+  };
+}
+
+export function loginFailed(errorMessage) {
+  return {
+    type: LOGIN_FAILED,
+    error: errorMessage,
+  };
+}
+
+export function loginSuccess(user) {
+  return {
+    type: LOGIN_SUCCESS,
+    user,
+  };
+}
+
+export function login(email, password) {
+  return (dispatch) => {
+    dispatch(startLogin());
+    const url = APIUrls.login();
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: getFormBody({ email, password }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log('data', data);
+        if (data.success) {
+          //dispatch action to save user
+          dispatch(loginSuccess());
+          return;
+        }
+        dispatch(loginFailed(data.message));
+      });
+  };
+}
+
+export function authenticateUser(user){
     return {
-        type:LOGIN_START,
+        type:AUTHENTICATE_USER,
+        user
     };
-};
+}
 
-export function login(email,password){
-    return (dispatch) => {
-        const url = APIUrls.login();
-        fetch(url,{
-            method: 'POST',
-            headers:{
-                'Content-Types' : 'application/x-www-form-urlencoded'
-            },
-            body:getFormBody({email,password}),
-        });
-    }
+export function logoutUser(){
+    return {
+        type:LOG_OUT
+    };
+}
+
+export function signup(email, password, confirmPassword, name) {
+  return (dispatch) => {
+    const url = APIUrls.signup();
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: getFormBody({
+        email,
+        password,
+        confirmPassword,
+        name,
+      }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        if (data.success) {
+        // error in getting the token
+        //   localStorage.setItem('token', data.data.token);
+          dispatch(signupSuccessful(data.data.user));
+          return;
+        }
+        dispatch(signupFailed(data.message));
+      });
+  };
+}
+
+export function startSignup() {
+  return {
+    type: SIGNUP_START,
+  };
+}
+
+export function signupFailed(error) {
+  return {
+    type: SIGNUP_FAILED,
+    error,
+  };
+}
+
+export function signupSuccessful(user) {
+  return {
+    type: SIGNUP_SUCCESS,
+    user
+  };
 }
--- src/helpers/urls.js
+++ src/helpers/urls.js
@@ -1,7 +1,7 @@
 const API_ROOT = 'http://localhost:8000/api/v1';
 
 export const APIUrls = {
-    login: () => `${API_ROOT}/users/create-session`,
-    signup : () => `${API_ROOT}/users/signup`,
-    fetchPosts: ()=>`${API_ROOT}/posts`,
-}
+  login: () => `${API_ROOT}/users/login`,
+  signup: () => `${API_ROOT}/users/signup`,
+  fetchPosts: () => `${API_ROOT}/posts`,
+};
